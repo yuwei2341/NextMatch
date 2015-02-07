14abec34c13c4945764fccac76554470530ab0b2
Yuwei Liu <yuwei2341@hotmail.com>
2015-02-07 09:27:54 -0800

    Move all file loading to view.py from run.py; test if the MySQL server probelm happens again

--- app/views.py
+++ app/views.py
@@ -1,13 +1,23 @@
 from flask import render_template, request
-from flask import current_app
 from app import app
+import pickle
 from PredictMatchAll import *
 
+# Load config file for database
+app.config.from_pyfile("../aws.cfg")
+# Load the database
+db = mdb.connect(user = app.config['DB_USER'], host = app.config['DB_HOST'], 
+    passwd = app.config['DB_PASS'], db = app.config['DB_NAME'], charset = 'utf8')
 
 @app.route('/')
 @app.route('/input')
 def input():
-	return render_template("input.html", title = 'Home', teams = current_app.teams)
+    # Load team names
+    cur = db.cursor()
+    cur.execute("show tables;")
+    query_results = cur.fetchall()
+    teams = [ii[0].replace('_', ' ').title() for ii in query_results]
+    return render_template("input.html", title = 'Home', teams = teams)
 	
 @app.route('/output', methods = ['GET'])
 def output():
@@ -15,8 +25,11 @@ def output():
     yourName = request.args.get('Item_1')
     tgtName = request.args.get('Item_2')
 
+    # Load team models
+    teamModels = pickle.load(open('./data/teamModels.p', "rb")) # teamModels contain log models for all teams
+
     # Compute chance of winning and recommended actions
-    odds, oddsNew, actions = PredictMatch(yourName, tgtName, current_app.teamModels, current_app.db) 
+    odds, oddsNew, actions = PredictMatch(yourName, tgtName, teamModels, db) 
     teamRec = {'yourName': yourName, 'tgtName': tgtName, 'odds': odds, 'oddsNew': oddsNew, 'actions': actions}
 
     return render_template("output.html", title = 'Home', teamRec = teamRec)
--- run.py
+++ run.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python
 from app import app
-import pymysql as mdb
-import pickle
-
-# Load config file for database
-app.config.from_pyfile("aws.cfg")
-
-## Load some data here before users hit the website
-
-# Load the database: was in views
-app.db = mdb.connect(user = app.config['DB_USER'], host = app.config['DB_HOST'], 
-	passwd = app.config['DB_PASS'], db = app.config['DB_NAME'], charset = 'utf8')
-
-# Load team names: was in views - input()
-cur = app.db.cursor()
-cur.execute("show tables;")
-query_results = cur.fetchall()
-app.teams = [ii[0].replace('_', ' ').title() for ii in query_results]
-
-# Load team models: was in views - output()
-app.teamModels = pickle.load(open('./data/teamModels.p', "rb")) # teamModels contain log models for all teams
 
 if __name__ == "__main__":
     app.run(debug = True, host = '0.0.0.0', port = 80)
