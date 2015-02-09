79e74ffe343cc9ebbeb83b00d037a9dfb341080b
Yuwei Liu <yuwei2341@hotmail.com>
2015-02-09 10:20:29 -0800

    Put database connections inside each page; hopefully this will solve the connection problem...

--- app/views.py
+++ app/views.py
@@ -1,22 +1,29 @@
 from flask import render_template, request
 from app import app
 import pickle
+import pymysql as mdb
 from PredictMatchAll import *
-
-# Load config file for database
-app.config.from_pyfile("../aws.cfg")
-# Load the database
-db = mdb.connect(user = app.config['DB_USER'], host = app.config['DB_HOST'], 
-    passwd = app.config['DB_PASS'], db = app.config['DB_NAME'], charset = 'utf8')
 
 @app.route('/')
 @app.route('/input')
 def input():
     # Load team names
-    cur = db.cursor()
-    cur.execute("show tables;")
-    query_results = cur.fetchall()
-    teams = [ii[0].replace('_', ' ').title() for ii in query_results]
+    try:
+        # Load config file for database 
+        app.config.from_pyfile("../aws.cfg")
+        db = mdb.connect(user = app.config['DB_USER'], host = app.config['DB_HOST'], 
+        passwd = app.config['DB_PASS'], db = app.config['DB_NAME'], charset = 'utf8')
+        cur = db.cursor()
+        cur.execute("show tables;")
+        query_results = cur.fetchall()
+        teams = [ii[0].replace('_', ' ').title() for ii in query_results]
+    except Exception as e:
+        print e.message
+        text_file = open("error1.txt", "a")
+        text_file.write(e.message)
+        text_file.close()
+        #trying to reconnect
+
     return render_template("input.html", title = 'Home', teams = teams)
 	
 @app.route('/output', methods = ['GET'])
@@ -29,9 +36,18 @@ def output():
     teamModels = pickle.load(open('./data/teamModels.p', "rb")) # teamModels contain log models for all teams
 
     # Compute chance of winning and recommended actions
-    odds, oddsNew, actions = PredictMatch(yourName, tgtName, teamModels, db) 
+    try:
+        # Load config file for database 
+        app.config.from_pyfile("../aws.cfg")
+        db = mdb.connect(user = app.config['DB_USER'], host = app.config['DB_HOST'], 
+        passwd = app.config['DB_PASS'], db = app.config['DB_NAME'], charset = 'utf8')
+        odds, oddsNew, actions = PredictMatch(yourName, tgtName, teamModels, db)
+    except Exception as e:
+        print e.message
+        text_file = open("error2.txt", "a")
+        text_file.write(e.message)
+        text_file.close()  
     teamRec = {'yourName': yourName, 'tgtName': tgtName, 'odds': odds, 'oddsNew': oddsNew, 'actions': actions}
-
     return render_template("output.html", title = 'Home', teamRec = teamRec)
 
 @app.route('/contact')
